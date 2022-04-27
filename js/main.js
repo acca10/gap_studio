52250da2bda5b5720cce22269076cf2b455930aa
acca10 <77087703+acca10@users.noreply.github.com>
2022-04-27 13:24:27 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,3 +9,77 @@ $(function () {
     }
   });
 });
+
+// $(function(){
+//   $(".btn-gnavi").on("click", function(){
+//       // ハンバーガーメニューの位置を設定
+//       var rightVal = 0;
+//       if($(this).hasClass("open")) {
+//           // 位置を移動させメニューを開いた状態にする
+//           rightVal = -300;
+//           // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+//           $(this).removeClass("open");
+//       } else {
+//           // メニューを開いたら次回クリック時は閉じた状態になるよう設定
+//           $(this).addClass("open");
+//       }
+//       $("#global-navi").stop().animate({
+//           right: rightVal
+//       }, 200);
+//   });
+// });
+
+
+$(document).ready(function () {
+  $("#topBtn, #menu-sp").hide(); //ボタンを非表示にする
+  $(window).on("scroll", function () {
+    const sliderHeight = $(".mv").height();
+      if (sliderHeight - 30 < $(this).scrollTop()) { 
+          $("#topBtn, #menu-sp").fadeIn("fast"); //ボタンがフェードインする
+      } else {
+          $("#topBtn, #menu-sp").fadeOut("fast"); //ボタンがフェードアウトする
+      }
+      scrollHeight = $(document).height(); //ドキュメントの高さ 
+      scrollPosition = $(window).height() + $(window).scrollTop(); //現在地 
+      footHeight = $("footer").innerHeight(); //止めたい位置の高さ(footer)
+      if (scrollHeight - scrollPosition <= footHeight) { //ドキュメントの高さと現在地の差がfooterの高さ以下の時
+          $("#topBtn, #menu-sp").css({
+              "position": "absolute", //pisitionをabsoluteに変更
+          });
+      } else { //それ以外の場合は
+          $("#topBtn, #menu-sp").css({
+              "position": "fixed", //固定表示
+          });
+      }
+  });
+// $(document).ready(function () {
+//   $("#topBtn menu-sp").hide(); //ボタンを非表示にする
+//   $(window).on("scroll", function () {
+//       if ($(this).scrollTop() > 100) { //ページの上から100pxスクロールした時
+//           $("#topBtn menu-sp").fadeIn("fast"); //ボタンがフェードインする
+//       } else {
+//           $("#topBtn menu-sp").fadeOut("fast"); //ボタンがフェードアウトする
+//       }
+//       scrollHeight = $(document).height(); //ドキュメントの高さ 
+//       scrollPosition = $(window).height() + $(window).scrollTop(); //現在地 
+//       footHeight = $("footer").innerHeight(); //止めたい位置の高さ(footer)
+//       if (scrollHeight - scrollPosition <= footHeight) { //ドキュメントの高さと現在地の差がfooterの高さ以下の時
+//           $("#topBtn menu-sp").css({
+//               "position": "absolute", //pisitionをabsoluteに変更
+//           });
+//       } else { //それ以外の場合は
+//           $("#topBtn menu-sp").css({
+//               "position": "fixed", //固定表示
+//           });
+//       }
+//   });
+
+
+//スムーススクロール設定
+  $('#topBtn').click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 800); //スムーススクロールの速度
+      return false;
+  });
+});
